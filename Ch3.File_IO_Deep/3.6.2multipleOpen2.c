caffad8466acaf9d5a00438c70ab721a2630484b
YOGA <wjh15875344277@outlook.com>
2023-02-28 16:58:07 +0800

    test multiple open

---
+++ Ch3.File_IO_Deep/3.6.2multipleOpen2.c
@@ -0,0 +1,72 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void)
+{
+    char buffer[4];
+    int fd1, fd2;
+    int ret;
+
+
+    /* 创建新文件 test_file 并打开 */
+    fd1 = open("./test_file",
+                O_RDWR | O_CREAT | O_EXCL,
+                S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (-1 == fd1)
+    {
+        perror("open error");
+        exit(-1);
+    }
+    
+    /* 再次打开 test_file 文件 */
+    fd2 = open("./test_file", O_RDWR);
+    if (-1 == fd2)
+    {
+        perror("open error");
+        ret = -1;
+        goto err1;
+    }
+    
+    /* 通过 fd1 文件描述符写入 4 个字节数据 */
+    buffer[0] = 0x11;
+    buffer[1] = 0x22;
+    buffer[2] = 0x33;
+    buffer[3] = 0x44;
+    ret = write(fd1, buffer, 4);
+    if (-1 == ret)
+    {
+        perror("write error");
+        goto err2;
+    }
+    
+    /* 将读写位置偏移量移动到文件头 */
+    ret = lseek(fd2, 0, SEEK_SET);
+    if (-1 == ret)
+    {
+        perror("lseek error");
+        goto err2;
+    }
+    
+    /* 读取数据 */
+    memset(buffer, 0x00, sizeof(buffer));
+    ret = read(fd2, buffer, 4);
+    if (-1 == ret)
+    {
+        perror("read error");
+        goto err2;
+    }
+    printf("0x%x 0x%x 0x%x 0x%x\n", buffer[0], buffer[1],
+                                    buffer[2], buffer[3]);
+    ret = 0;
+    
+/* 关闭文件 */
+err2:
+    close(fd2);
+err1:
+    close(fd1);
+    exit(ret);
+}

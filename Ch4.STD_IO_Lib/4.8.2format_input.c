8eeb67098fe6ae68d6c120580e2808fce9ec1ab4
Wong-Xian <wjh15875344277@outlook.com>
2023-03-04 20:20:59 +0800

    自行测试格式化输入中的*号和m

---
+++ Ch4.STD_IO_Lib/4.8.2format_input.c
@@ -0,0 +1,27 @@
+/*
+测试【标准输入函数】的参数使用。
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    // define var
+    int d1,d2;
+    char s1[20]={"hello world!"};
+    char* buf;
+
+    // 测试%*d跳过某个输入
+    // printf("输入3个数，第二个数将不会被保存。\n");
+    // scanf("%d %*d %d", &d1, &d2);
+    // printf("输入的第一个数是：%d，输入的第三个数是：%d。\n", d1, d2);
+
+    // 测试%ms
+    scanf("%ms", &buf);
+    printf("%s\n", buf);
+    // 注意，不能有空格，因为空格会被认为是字符串的结束。
+    free(buf);  // 用完要free掉内存空间
+
+    return 0;
+}

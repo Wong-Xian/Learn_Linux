60d3300e051f71e262e7ea51508c150988272e26
Wong-Xian <wjh15875344277@outlook.com>
2023-03-04 22:07:29 +0800

    学习fsync

---
+++ Ch4.STD_IO_Lib/4.9.1fsync.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+#define BUF_SIZE 4096
+#define READ_FILE "./rfile"
+#define WRITE_FILE "./wfile"
+
+static char buf[BUF_SIZE];
+
+int main(void)
+{
+    int rfd, wfd;
+    size_t size;
+
+    /* 打开源文件 */
+    if (0 > (rfd=open(READ_FILE, O_RDONLY)))
+    {
+        perror("open error");
+        exit(-1);
+    }
+
+    /* 打开目标文件 */
+    if (0 > (wfd=open(WRITE_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0664)))
+    {
+        perror("open error");
+        exit(-1);
+    }
+
+    /* 拷贝数据 */
+    while(0 < (size = read(rfd, buf, BUF_SIZE)))
+    {
+        write(wfd, buf, size);
+    }
+    
+    /* 对目标文件执行 fsync 同步 */
+    fsync(wfd);
+    
+    /* 关闭文件退出程序 */
+    close(rfd);
+    close(wfd);
+    exit(0);
+}

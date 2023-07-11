c9b4a96ccb5a2c42119631186f159a468fce1a5d
YOGA <wjh15875344277@outlook.com>
2023-07-11 16:09:09 +0800

    adjust file

---
+++ Linux_C_Program/Ch3.File_IO_Deep/3.10.1fcntl_dupfd.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    int fd1, fd2;
+    int ret;
+
+
+    /* 打开文件 test_file */
+    fd1 = open("./test_file", O_RDONLY);
+    if (-1 == fd1) 
+    {
+        perror("open error");
+        exit(-1);
+    }
+
+
+    /* 使用 fcntl 函数复制一个文件描述符 */
+    fd2 = fcntl(fd1, F_DUPFD, 0);   // the returned fd2 will be >= 0(the 3rd arg).
+    if (-1 == fd2)
+    {
+        perror("fcntl error");
+        ret = -1;
+        goto err;
+    }
+    printf("fd1: %d\nfd2: %d\n", fd1, fd2);
+    ret = 0;
+    close(fd2);
+
+err:
+    /* 关闭文件 */
+    close(fd1);
+    exit(ret);
+}
